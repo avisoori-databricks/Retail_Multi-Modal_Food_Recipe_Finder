9b75ae0ac857b4a4bccfcedb985f47baecc8d796
avisoori-databricks <98932637+avisoori-databricks@users.noreply.github.com>
2022-08-31 14:58:01 -0400

    Setting up context

---
+++ Part 0_ Introduction and overview of architecture.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## 0. Motivation: Multimedia search and even multimodal search
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Similarity search is a better fit for many use cases in the retail industry than naive classification, as there could be countably infinite classes for objects. Recommenders based on embeddings from categorical variables, text, images and audio as well as media semantic/ similarity search can be achieved with the  foundation model-> embedding-> downstream application pattern.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This series of notebooks details the process of building a recipe recommender using the OpenAI CLIP-ViT, MLFlow, FAISS and Databricks Serverless serving  
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC Will add architecture diagram shortly
+
+# COMMAND ----------
+
+
